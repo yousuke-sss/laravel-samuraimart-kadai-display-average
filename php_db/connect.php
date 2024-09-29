ed2f993a69547bb2120e50d92be1983e57224f89
yousuke-sss <you.sekine130702@outlook.jp>
2024-09-29 12:28:40 +0900

    kadai

--- php_db/connect.php
+++ php_db/connect.php
@@ -11,10 +11,14 @@
          <?php
          $dsn = 'mysql:dbname=php_db;host=localhost;charset=utf8mb4';
          $user = 'root';
-        // MAMPを利用しているMacユーザーの方は、''ではなく'root'を代入してください
          $password = '';
  
-         $pdo = new PDO($dsn, $user, $password);
+         try {
+            $pdo = new PDO($dsn, $user, $password);
+            echo "データベースに接続しました！";
+        } catch (PDOException $e) {
+            echo '接続失敗: ' . $e->getMessage();
+        }
          
          ?>
      </p>
