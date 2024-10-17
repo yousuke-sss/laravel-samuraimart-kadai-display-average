69118598d31fd4e231a5d2a04b858c0dbbfc191b
yousuke-sss <you.sekine130702@outlook.jp>
2024-10-17 22:41:04 +0900

    10/17kadai

--- laravel-basic-kadai/app/Http/Controllers/PostController.php
+++ laravel-basic-kadai/app/Http/Controllers/PostController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use App\Models\Post;
 
 class PostController extends Controller{ 
 
@@ -15,5 +16,12 @@ class PostController extends Controller{
         // 変数$postsをposts/index.blade.phpファイルに渡す
         return view('posts.index', compact('posts'));
     }    
-    
+
+    public function show($id) {
+        // URL'/products/{id}'の'{id}'部分と主キー（idカラム）の値が一致するデータをproductsテーブルから取得し、変数$productに代入する
+        $post = Post::find($id);
+
+        // 変数$productをposts/show.blade.phpファイルに渡す
+        return view('posts.show', compact('post'));
+    }    
 }
