24b73d731c4bcc4c0a91ea0de5c832e17701f191
yousuke-sss <you.sekine130702@outlook.jp>
2024-11-04 10:49:04 +0900

    11.4課題

--- laravel-posting-app/database/factories/PostFactory.php
+++ laravel-posting-app/database/factories/PostFactory.php
@@ -17,10 +17,9 @@ class PostFactory extends Factory
     public function definition(): array
     {
         return [
-       'user_id' => 2,
-       'title' => fake()->realText(20),
-       'content' => fake()->realText(200),
-       
+            'user_id' => 1, // usersテーブルにidカラムの値が1のユーザーが存在することが前提
+            'title' => fake()->realText(20, 5),
+            'content' => fake()->realText(200, 5)       
 
         ];
     }
